b8dc0bf7a8baed8509ad11a8369d778a96ffa651
anita00001 <sharma.anita00001@gmail.com>
2023-09-22 11:51:51 +0545

    Add Validation in model User and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,11 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def most_recent_comments
-    posts.order(created_at: :desc).limit(5)
+    comments.order(created_at: :desc).limit(5)
   end
 
   after_save :update_post_counter
