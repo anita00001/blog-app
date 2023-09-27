050a892e948af58a6146c27670b66416dde1f753
anita00001 <sharma.anita00001@gmail.com>
2023-09-27 10:25:32 +0545

    Spec for controllers: Users and Posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,46 @@
+# spec/controllers/posts_controller_spec.rb
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_index_path
+RSpec.describe 'Posts', type: :request do
+  describe 'GET #index' do
+    it 'returns a 200 OK status' do
+      user = User.create(name: 'Test User')
+      get "/users/#{user.id}/posts"
       expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      user = User.create(name: 'Test User')
+      get "/users/#{user.id}/posts"
+      expect(response).to render_template('index')
+    end
+
+    it 'renders correct placeholder text' do
+      user = User.create(name: 'Test User')
+      get "/users/#{user.id}/posts"
+      expect(response.body).to include('Welcome to the Post page!')
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      user = User.create(name: 'Test User')
+      get "/users/#{user.id}/posts"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      user = User.create(name: 'Test User')
+      post = user.posts.create(title: 'Test Post', text: 'Test Content')
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to render_template('show')
+    end
+
+    it 'renders correct placeholder text' do
+      user = User.create(name: 'Test User')
+      post = user.posts.create(title: 'Test Post', text: 'Test Content')
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response.body).to include('Here is a list of posts for a given user.')
     end
   end
 end
