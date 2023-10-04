7d68d056f73bca84260d30a9c7273cd0265a7f11
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-04 21:11:43 +0530

    Revert "Configure devise"
    This reverts commit 9c48f9e638295d5f6190bd63c4621ad753a6fc41.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:email, :password, :password_confirmation, :name)
-    end
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.first
   end
 end
