26b40f182c41005e2bc3d4720041067696d32fed
anita00001 <sharma.anita00001@gmail.com>
2023-10-06 16:25:58 +0545

    define role: admin/user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,12 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  enum role: [:user, :admin]
+
+  def admin?
+    role == 'admin'
+  end
+
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
